6ceae3e140e7a32cb21ab900f68f7558a30ac1fa
Ibrahim <alzuabi.ibr@gmail.com>
2020-12-28 16:03:04 +0200

    qwe

--- Utils/Temp.cs
+++ Utils/Temp.cs
@@ -9,6 +9,7 @@ namespace WindowsFormsApp1.Utils
 {
     class Temp
     {
+        //
         public string GetTemporaryDirectory()
         {
             string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
